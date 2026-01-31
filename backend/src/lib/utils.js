4bf203cb7e3cc6b4f9e010bf4489c389917c7457
akku213 <akankshashrivastav2103@gmail.com>
2026-01-31 23:58:55 +0530

    Final commit

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -4,12 +4,13 @@ export const generateToken = (userId, res) =>{
     const token = jwt.sign({userId}, process.env.JWT_SECRET, {
         expiresIn: '7d',
     })
-    
-    res.cookie('jwt', token, {
-        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days MS
-        httpOnly: true,  // prevent XSS attacks cross-site scripting attacks
-        sameSite: 'strict', // CSRF attacks cross-site request forgery attacks
-        secure: process.env.NODE_ENV !== 'development', // set to true in production
-    })
+    const cookieOptions = {
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+        httpOnly: true,
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        secure: process.env.NODE_ENV === 'production',
+    }
+
+    res.cookie('jwt', token, cookieOptions)
     return token;
 };
